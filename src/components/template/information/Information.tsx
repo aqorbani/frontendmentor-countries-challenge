c9159c0cba14acdbf7d858e76431ed42d32e63aa
AmirHossein <a.qorbani.eng@gmail.com>
2024-05-22 21:56:32 +0330

    get country data successfully

--- src/components/template/information/Information.tsx
+++ src/components/template/information/Information.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Information = ({ name }: { name: string }) => {
-  return <div>{name}</div>;
+  const [country, setCountry] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (country.length <= 0) {
+      getCountriesData();
+    }
+  });
+
+  const getCountriesData = async () => {
+    setLoading(false);
+    const res: any = await axios.get(
+      "https://restcountries.com/v3.1/name/" + name + "?fullText=true"
+    );
+    setCountry(res.data);
+    console.log(res);
+    setLoading(true);
+  };
+
+  return <div>{!loading ? <p>loading</p> : <p>{name}</p>}</div>;
 };
 
 export default Information;
