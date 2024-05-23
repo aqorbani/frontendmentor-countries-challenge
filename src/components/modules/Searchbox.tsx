6f96dd7489228e8c5f80ffd88d552fa21ffdf40b
AmirHossein <a.qorbani.eng@gmail.com>
2024-05-24 01:48:57 +0330

    add delay to start search function by user typing

--- src/components/modules/Searchbox.tsx
+++ src/components/modules/Searchbox.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 
 const Searchbox = ({
@@ -8,14 +9,24 @@ const Searchbox = ({
   search: string;
   setSearch: Function;
 }) => {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const delayonChange = setTimeout(() => {
+      setSearch(text);
+    }, 2000);
+
+    return () => clearTimeout(delayonChange);
+  }, [text]);
+
   return (
     <div className="flex items-center bg-white dark:bg-gray-700  text-gray-400 dark:text-white p-2 my-2 rounded outline-transparent">
       <IoSearch className="mr-1 ml-3 text-[2.5vw] md:text-[1.2vw]" />
       <input
         type="text"
         placeholder="Search for a country..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
         className="text-[2vw] md:text-[0.8vw] p-2 w-full md:w-56 bg-white dark:bg-gray-700  text-gray-400 dark:text-white dark:placeholder:text-white"
       />
     </div>
